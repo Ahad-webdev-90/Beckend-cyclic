9c096a465a082e38a1b17e11932608933f8ac484
Ahad-webdev-90 <ahadmemon718@gmail.com>
2023-12-31 16:24:16 +0500

    Up To Date Work

--- route/TeamRoute.js
+++ route/TeamRoute.js
@@ -6,11 +6,11 @@ router.get('/', TeamController.getAllTeamsWithMembers)
 router.get('/:teamId/members', TeamController.getTeamMembers)
 router.post('/create', TeamController.createTeam);
 router.post('/sendMessage', async (req, res) => {
-    const { team, senderId, content } = req.body;
-    let obj = { team, senderId, content };
+    const { teamId, senderId, content } = req.body;
+    let obj = { teamId, senderId, content };
     let errArr = [];
-    if (!obj.team) {
-        errArr.push('Please select team ! , team is not define');
+    if (!obj.teamId) {
+        errArr.push('Please select teamId ! , teamId is not define');
     }
     if (!obj.senderId) {
         errArr.push('Sender not found');
@@ -25,7 +25,7 @@ router.post('/sendMessage', async (req, res) => {
             data: errArr,
         });
     }
-    TeamController.sendMessage(req.app.io, { team, content, senderId });
+    TeamController.sendMessage(req.app.io, { teamId, content, senderId });
     res.status(200).json({ success: true, message: content, senderId });
 });
 
